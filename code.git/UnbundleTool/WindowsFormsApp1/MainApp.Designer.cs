767e34d7a5011971c22148a18d8e1d8f22d94793
rudolfearle <rudolf.earle@gmail.com>
2018-08-05 09:43:06 +0200

    debug round 1 on unbundle tool

---
+++ code.git/UnbundleTool/WindowsFormsApp1/MainApp.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainApp));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(905, 564);
+            this.splitContainer1.SplitterDistance = 51;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(905, 39);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "Asset Vault";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(90, 36);
+            this.toolStripButton1.Tag = "1";
+            this.toolStripButton1.Text = "ISU Vualt";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(112, 36);
+            this.toolStripButton2.Tag = "2";
+            this.toolStripButton2.Text = "Lookup Vault";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(100, 36);
+            this.toolStripButton3.Tag = "3";
+            this.toolStripButton3.Text = "HCM Vault";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(105, 36);
+            this.toolStripButton4.Tag = "4";
+            this.toolStripButton4.Text = "Assets Vault";
+            // 
+            // MainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 564);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainApp";
+            this.Text = "Unbundle Tool";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+    }
+}
---
+++ code.git/UnbundleTool/WindowsFormsApp1/MainApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainApp : Form
+    {
+        public MainApp()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            int But = Convert.ToInt16(e.ClickedItem.Tag.ToString());
+            if (splitContainer1.Panel2.Controls.Count > 0 )
+            {
+                splitContainer1.Panel2.Controls.RemoveAt(0);
+            }
+            switch (But)
+            {
+                case 1: //ISU
+                    Lookup_ISU_Vault obj = new Lookup_ISU_Vault();
+                    obj.TopLevel = false;
+                    splitContainer1.Panel2.Controls.Add(obj);
+                    obj.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                    obj.Dock = DockStyle.Fill;
+                    obj.Show();
+                    break;
+                case 2: // Vault
+                    Lookup_Vault obj1 = new Lookup_Vault();
+                    obj1.TopLevel = false;
+                    splitContainer1.Panel2.Controls.Add(obj1);
+                    obj1.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                    obj1.Dock = DockStyle.Fill;
+                    obj1.Show();
+                    break;
+                case 3: //HCM
+                    Lookup_HCM_Vault obj2 = new Lookup_HCM_Vault();
+                    obj2.TopLevel = false;
+                    splitContainer1.Panel2.Controls.Add(obj2);
+                    obj2.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                    obj2.Dock = DockStyle.Fill;
+                    obj2.Show();
+                    break;
+                case 4: //Assets
+                    Lookup_Assets_Vault obj3 = new Lookup_Assets_Vault();
+                    obj3.TopLevel = false;
+                    splitContainer1.Panel2.Controls.Add(obj3);
+                    obj3.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                    obj3.Dock = DockStyle.Fill;
+                    obj3.Show();
+                    break;
+            }
+        }
+    }
+}
